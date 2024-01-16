b9eb423fb4e6d80b7dd29efa8b5f212b49c974a5
Jayesh Thakur <jayesh.thakur@cognizant.com>
2024-01-16 19:00:39 +0530

    added def for Message

--- app/libs/definitions.ts
+++ app/libs/definitions.ts
@@ -3,3 +3,12 @@ export type User = {
   name: String;
   email: String;
 };
+
+export type Message = {
+  from: String;
+  to: String;
+  content: String;
+  timeSent: String;
+  timereceveied: String;
+  seen: Boolean;
+};
